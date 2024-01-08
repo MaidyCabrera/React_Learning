222a9d63024f8374363b3439f53cb5bad6775b44
Maidy Cabrera <m.cabrerap@uniandes.edu.co>
2024-01-07 19:50:45 -0500

    Feature: useEffect hook - forms

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { CounterApp } from './01-useState/CounterApp';
-import { CounterCustomHookApp } from './01-useState/CounterWithCustomHook';
+import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { CounterCustomHookApp } from './01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
 import './index.css'
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
-    <CounterCustomHookApp />
+    {/* <CounterCustomHookApp /> */}
+    <SimpleForm />
   </React.StrictMode>,
 )
